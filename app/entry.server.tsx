df392a8ccfa61be1f22155bf038d05dd8133bb7f
Ale Cavalcante <seu-email@exemplo.com>
2025-12-04 18:34:30 -0300

    fix: remover acesso a themeStore no entry.server (client-side state em server)

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -4,7 +4,6 @@ import { isbot } from 'isbot';
 import ReactDOMServer from 'react-dom/server';
 import { renderHeadToString } from 'remix-island';
 import { Head } from './root';
-import { themeStore } from '~/lib/stores/theme';
 
 export default async function handleRequest(
   request: Request,
@@ -31,7 +30,7 @@ export default async function handleRequest(
       controller.enqueue(
         new Uint8Array(
           new TextEncoder().encode(
-            `<!DOCTYPE html><html lang="en" data-theme="${themeStore.value}"><head>${head}</head><body><div id="root" class="w-full h-full">`,
+            `<!DOCTYPE html><html lang="en" data-theme="light"><head>${head}</head><body><div id="root" class="w-full h-full">`,
           ),
         ),
       );
